a7a33b047121e699d18ec0563f7284217d65d933
Carlos Bazan <c.bazan@mercapsoftware.com>
2025-09-22 23:51:34 -0300

    comentando comando saveAppVersion

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -85,15 +85,28 @@ Cypress.Commands.add("addAllItemsToCart", () => {
   });
 });
 
+// Cypress.Commands.add("saveAppVersion", () => {
+//     cy.get('div[class="footer_copy"]')
+//     .invoke('text')
+//     .then((text) => {
+//       const match = text.match(/\d{4}/); // busca un año (4 dígitos seguidos)
+//       const year = match ? match[0] : 'desconocido';
+
+//       cy.writeFile('version.txt', year);
+//       cy.log(`Guardando la versión ${year}`)
+//     });
+// });
+
 Cypress.Commands.add("saveAppVersion", () => {
-    cy.get('div[class="footer_copy"]')
+  cy.get('div.footer_copy')
     .invoke('text')
     .then((text) => {
-      const match = text.match(/\d{4}/); // busca un año (4 dígitos seguidos)
-      const year = match ? match[0] : 'desconocido';
+      const match = text.match(/\d{4}/);       // busca un año (4 dígitos)
+      const year = match ? parseInt(match[0], 10) : null;
+      const nextYear = year ? year + 1 : 2021;
 
-      cy.writeFile('version.txt', year);
-      cy.log(`Guardando la versión ${year}`)
+      cy.writeFile('version.txt', String(nextYear));
+      cy.log(`Guardando la versión ${nextYear}`);
     });
 });
 
